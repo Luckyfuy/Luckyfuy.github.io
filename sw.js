6142764e11114fc4ec5290751419d93db0d8b5a0
Luckyfuy <abcxlxcba@sina.com>
2019-10-18 21:55:21 +0800

    Site updated: 2019-10-18 21:55:13

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2019/08/31/MySQL-Installation/');
+  precacheUrls.push('/2019/09/07/Knapsack/');
 
   precacheUrls.push('/2019/10/02/Prime-And-GCD/');
 
@@ -32,7 +32,7 @@ var precacheUrls = [];
 
   precacheUrls.push('/2019/10/11/Change-Blog-Domain/');
 
-  precacheUrls.push('/2019/09/07/Knapsack/');
+  precacheUrls.push('/2019/08/27/Windows/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
