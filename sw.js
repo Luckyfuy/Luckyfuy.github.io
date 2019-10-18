f9a2ebdf40076993bc101d26bda9f481c59214c0
Luckyfuy <abcxlxcba@sina.com>
2019-10-18 21:58:56 +0800

    Site updated: 2019-10-18 21:58:47

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2019/09/07/Knapsack/');
+  precacheUrls.push('/2019/08/27/Windows/');
 
   precacheUrls.push('/2019/10/02/Prime-And-GCD/');
 
-  precacheUrls.push('/2019/10/07/Sparse-Table/');
-
   precacheUrls.push('/2019/10/11/Change-Blog-Domain/');
 
-  precacheUrls.push('/2019/08/27/Windows/');
+  precacheUrls.push('/2019/10/07/Sparse-Table/');
+
+  precacheUrls.push('/2019/08/31/MySQL-Installation/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
