a24d485784a87b90ab2c6b3632726571eaf3e2c7
Luckyfuy <abcxlxcba@sina.com>
2019-11-24 14:22:01 +0800

    Site updated: 2019-11-24 14:21:51

--- sw.js
+++ sw.js
@@ -24,13 +24,13 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2019/08/31/MySQL-Installation/');
+  precacheUrls.push('/2019/10/01/Shortest-Path/');
 
   precacheUrls.push('/2019/10/02/Prime-And-GCD/');
 
-  precacheUrls.push('/2019/11/03/PIP-Mirror/');
-
   precacheUrls.push('/2019/10/11/Change-Blog-Domain/');
+
+  precacheUrls.push('/2019/11/03/PIP-Mirror/');
 
   precacheUrls.push('/2019/11/17/CSP-J-2019-Trip/');
 
