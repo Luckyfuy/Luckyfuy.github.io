5995b46195af52093e5f786b0ec51505ead31e68
Luckyfuy <abcxlxcba@sina.com>
2019-11-30 16:25:33 +0800

    Site updated: 2019-11-30 16:25:22

--- sw.js
+++ sw.js
@@ -28,11 +28,11 @@ var precacheUrls = [];
 
   precacheUrls.push('/2019/10/02/Prime-And-GCD/');
 
-  precacheUrls.push('/2019/10/11/Change-Blog-Domain/');
-
-  precacheUrls.push('/2019/11/03/PIP-Mirror/');
-
   precacheUrls.push('/2019/11/30/Build-MkDocs-Material/');
+
+  precacheUrls.push('/2019/11/24/Proofs-in-Triangle/');
+
+  precacheUrls.push('/2019/11/17/CSP-J-2019-Trip/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
