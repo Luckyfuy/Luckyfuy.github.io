cd106b9ebe8677dd23414a938b673f65faa1b81b
Luckyfuy <abcxlxcba@sina.com>
2019-12-13 16:26:51 +0800

    Site updated: 2019-12-13 16:26:35

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2019/09/14/Check/');
+  precacheUrls.push('/2019/10/07/Sparse-Table/');
 
   precacheUrls.push('/2019/10/02/Prime-And-GCD/');
 
@@ -32,7 +32,7 @@ var precacheUrls = [];
 
   precacheUrls.push('/2019/11/03/PIP-Mirror/');
 
-  precacheUrls.push('/2019/10/11/Change-Blog-Domain/');
+  precacheUrls.push('/2019/12/13/The-Prophet/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
