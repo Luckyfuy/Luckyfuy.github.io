1a1329015d89a3b97bb0aaca4900792d599273ce
Luckyfuy <abcxlxcba@sina.com>
2020-03-01 22:31:55 +0800

    Site updated: 2020-03-01 22:31:39

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2019/10/01/Shortest-Path/');
+  precacheUrls.push('/2019/10/07/Sparse-Table/');
+
+  precacheUrls.push('/2019/11/17/CSP-J-2019-Trip/');
 
   precacheUrls.push('/2019/12/13/The-Prophet/');
 
-  precacheUrls.push('/2019/09/14/Check/');
+  precacheUrls.push('/2019/11/24/Proofs-in-Triangle/');
 
-  precacheUrls.push('/2019/05/11/1+1=2/');
-
-  precacheUrls.push('/2019/06/29/Basic-Mod-Packs/');
+  precacheUrls.push('/2019/11/30/Build-MkDocs-Material/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
