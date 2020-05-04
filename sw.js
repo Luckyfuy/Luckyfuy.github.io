3838e747daf0742f59a7f5e3821922bcd5826b8c
Luckyfuy <abcxlxcba@sina.com>
2020-05-04 12:43:46 +0800

    Site updated: 2020-05-04 12:43:29

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2019/08/27/Windows/');
+  precacheUrls.push('/2019/09/14/Check/');
 
   precacheUrls.push('/2019/12/13/The-Prophet/');
 
-  precacheUrls.push('/2019/11/03/PIP-Mirror/');
+  precacheUrls.push('/2019/10/07/Sparse-Table/');
 
-  precacheUrls.push('/2019/06/29/Basic-Mod-Packs/');
+  precacheUrls.push('/2019/03/16/Winter-Solstice/');
 
-  precacheUrls.push('/2019/10/02/Prime-And-GCD/');
+  precacheUrls.push('/2019/05/11/1+1=2/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
