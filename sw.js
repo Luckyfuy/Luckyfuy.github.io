1acf78ef652c06e5d814dd4f4b65b45e070bb22a
Luckyfuy <abcxlxcba@sina.com>
2020-07-26 16:23:11 +0800

    Site updated: 2020-07-26 16:22:56

--- sw.js
+++ sw.js
@@ -28,9 +28,9 @@ var precacheUrls = [];
 
   precacheUrls.push('/2020/07/19/The-Historical-Atlas-of-China/');
 
-  precacheUrls.push('/2019/10/11/Change-Blog-Domain/');
+  precacheUrls.push('/2019/10/07/Sparse-Table/');
 
-  precacheUrls.push('/2019/03/16/Winter-Solstice/');
+  precacheUrls.push('/2019/03/09/Build-Blog/');
 
   precacheUrls.push('/2019/05/11/1+1=2/');
 
