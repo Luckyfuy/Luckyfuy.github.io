ca8c5844f57b06504ac991d5295ca2b536ec0e21
Luckyfuy <abcxlxcba@sina.com>
2020-08-02 21:09:01 +0800

    Site updated: 2020-08-02 21:08:40

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2019/10/07/Sparse-Table/');
+  precacheUrls.push('/2019/10/11/Change-Blog-Domain/');
 
   precacheUrls.push('/2020/07/19/The-Historical-Atlas-of-China/');
 
-  precacheUrls.push('/2019/10/01/Shortest-Path/');
+  precacheUrls.push('/2019/11/17/CSP-J-2019-Trip/');
 
-  precacheUrls.push('/2019/03/09/Build-Blog/');
+  precacheUrls.push('/2019/03/16/Winter-Solstice/');
 
-  precacheUrls.push('/2019/06/29/Basic-Mod-Packs/');
+  precacheUrls.push('/2019/05/11/1+1=2/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
