0fe441a9e0ab7696653dbde5f2292a7c6eae051e
Luckyfuy <abcxlxcba@sina.com>
2021-04-24 11:56:18 +0800

    Site updated: 2021-04-24 11:55:46

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2019/11/08/Windows-Shortcut-Keys/');
+  precacheUrls.push('/2019/11/30/Build-MkDocs-Material/');
 
   precacheUrls.push('/2020/11/14/Mao-Selected-Works-2/');
 
@@ -32,7 +32,7 @@ var precacheUrls = [];
 
   precacheUrls.push('/2021/04/04/Timeline-of-Chinese-and-Foreign-History/');
 
-  precacheUrls.push('/2021/02/26/Taiwan-Version-Historical-Atlas-of-China/');
+  precacheUrls.push('/2021/03/06/Mao-Selected-Works-3/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
