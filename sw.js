0f71d5a518970fb53fcda90676c07142745b38c6
Luckyfuy <abcxlxcba@sina.com>
2021-05-22 00:27:31 +0800

    Site updated: 2021-05-22 00:27:13

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2021/03/06/Mao-Selected-Works-3/');
+  precacheUrls.push('/2021/02/26/Taiwan-Version-Historical-Atlas-of-China/');
 
   precacheUrls.push('/2020/11/14/Mao-Selected-Works-2/');
 
