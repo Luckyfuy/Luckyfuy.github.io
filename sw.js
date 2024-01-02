9fe223498e654cbde3df6c912b214ad1f8df0202
Luckyfuy <abcxlxcba@sina.com>
2024-01-02 23:01:08 +0800

    Site updated: 2024-01-02 23:00:30

--- sw.js
+++ sw.js
@@ -24,13 +24,13 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
+  precacheUrls.push('/2018/12/30/Hello-World/');
+
   precacheUrls.push('/2019/03/09/Blog-Building/');
 
   precacheUrls.push('/2019/03/22/Learn-Python/');
 
-  precacheUrls.push('/2018/12/30/Hello-World/');
-
-  precacheUrls.push('/2019/04/12/Redstone-Circuit/');
+  precacheUrls.push('/2019/06/29/Basic-Mod-Pack/');
 
   precacheUrls.push('/2019/03/30/Python-Data-Type/');
 
