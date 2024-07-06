f505f2f59cc505e33af552c62ff242b1d70cc33b
Luckyfuy <mcluckyfuy@gmail.com>
2024-07-07 00:35:15 +0800

    Site updated: 2024-07-07 00:34:41

--- sw.js
+++ sw.js
@@ -26,11 +26,11 @@ var precacheUrls = [];
 
   precacheUrls.push('/2018/12/30/Hello-World/');
 
-  precacheUrls.push('/2019/03/30/Python-Data-Type/');
-
   precacheUrls.push('/2019/03/22/Learn-Python/');
 
   precacheUrls.push('/2019/03/09/Blog-Building/');
+
+  precacheUrls.push('/2019/03/30/Python-Data-Type/');
 
   precacheUrls.push('/2019/06/29/Basic-Mod-Pack/');
 
